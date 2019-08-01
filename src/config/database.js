9668f87e9a731d96f440d4968e1b1bf9f8d36aa5
Alex Mariot Bender <alexbenderkv@gmail.com>
2019-08-01 14:30:45 -0300

    GoBarber First Push

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: "postgres",
+  host: "127.0.0.1",
+  username: "postgres",
+  password: "123",
+  database: "postgres",
+  operatorAliases: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+};
